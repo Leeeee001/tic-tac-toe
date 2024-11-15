5fa35a8bc56e2928e7611f129e5b134e282c9bf9
Tanmoy <tanmoychakraborty0041383@gmail.com>
2024-11-15 20:33:45 +0530

    changes all

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let newGame = document.querySelector(".newBtn");
 let msgContainer = document.querySelector(".win-container");
 let msg = document.querySelector(".msg");
 
+
+let isDraw = true;
 let turnO = true;  //playerX, player0
 
 const winPatterns = [  //winning patterns
@@ -23,9 +25,11 @@ boxes.forEach((box) => {
         console.log("box clicked");
         if(turnO) {
             box.innerText = "O";
+            box.classList.add("txtColor");
             turnO = false;
         } else {
             box.innerText = "X";
+            box.classList.remove("txtColor");
             turnO = true;
         }
         box.disabled = true;
@@ -38,7 +42,7 @@ const enableBoxes = () => {
     for(let box of boxes) {
         box.disabled = false;
         box.innerText = "";
-       
+        box.classList.remove("txtColor");
     }
 }
 
@@ -52,12 +56,14 @@ const disabledBoxes = () => {
 const showWinner = (winner) => {
     msg.innerText = `🎉Congratulations🎉\n Winner is player ${winner}`;
     msgContainer.classList.remove("hide");
+    newGame.classList.add("glow");
     disabledBoxes();
-    
+    reset.disabled = true;
 };
 
 
 const checkWinner = () => {
+    let isDraw = true;
     for(let pattern of winPatterns) {
         let pos1 = boxes[pattern[0]].innerText;
         let pos2 = boxes[pattern[1]].innerText;
@@ -65,8 +71,18 @@ const checkWinner = () => {
         if(pos1 != "" && pos2 != "" && pos3 != "") {
             if(pos1 === pos2 && pos2 === pos3) {
                 showWinner(pos1);
+                return;
             }
         }
+    }
+    boxes.forEach((box) => {
+        if (box.innerText === "") {
+            isDraw = false; 
+        }
+    });
+
+    if (isDraw) {
+        showDraw(); 
     }
 };
 
@@ -75,6 +91,15 @@ const resetGame = () => {
     turnO = true;
     enableBoxes();
     msgContainer.classList.add("hide");
+    reset.disabled = false;
+    newGame.classList.remove("glow");
+};
+
+const showDraw = () => {
+    msg.innerText = "🤝 Match Draw! Try Again!";
+    msgContainer.classList.remove("hide");
+    disabledBoxes();
+    reset.disabled = true; 
 };
 
 newGame.addEventListener("click", resetGame);
